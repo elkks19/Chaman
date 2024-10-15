dc59ee6b99d45b656a8a26390a2ab3ae316cfebf
elkks19 <rafafabiani1909@gmail.com>
2024-10-14 22:35:28 -0400

    animacion al salir

--- scenes/menu/menu.gd
+++ scenes/menu/menu.gd
@@ -1,6 +1,7 @@
 extends Control
 @onready var container = $VBoxContainer
-@onready var escogerAudio = $VBoxContainer/Escoger
+@onready var quitAudio = $VBoxContainer/Quit
+@onready var startAudio = $VBoxContainer/Start
 @onready var hoverAudio = $VBoxContainer/Hover
 
 @onready var startButton = $VBoxContainer/StartButton
@@ -11,16 +12,19 @@ func _ready():
 	startButton.grab_focus()
 
 func _on_start_button_pressed():
-	escogerAudio.play()
-	var fade_out_options = SceneManager.create_options(1.0, "scribbles", 0.2, true)
+	startAudio.play()
+	var fade_out_options = SceneManager.create_options(1.0, "fade", 0.2, true)
 	var fade_in_options = SceneManager.create_options(1.0, "crooked_tiles", 0.2, true)
 	var general_options = SceneManager.create_general_options(Color(0, 0, 0), 0, false, true)
 	SceneManager.change_scene("level1", fade_out_options, fade_in_options, general_options)
 
 
 func _on_quit_button_pressed():
-	self.set_process_input(false)
-	escogerAudio.play()
+	quitAudio.play()
+	$FadeOut/AnimationPlayer.play("fade_out")
+
+func _on_quit_finished():
+	get_tree().quit()
 
 func _on_start_button_mouse_entered() -> void:
 	hoverAudio.play()
